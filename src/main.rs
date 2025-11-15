9c16d5e6ef302c9672e45c1b78a77e395a901bd7
Smelly Shrimp <tymek.lamza@op.pl>
2025-11-15 23:37:36 +0100

    added Lexer, Tok & TokKind

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,35 @@
+use std::str::Chars;
+
+struct Tok {
+    kind: TokKind,
+    span: (usize, usize),
+}
+
+enum TokKind {
+    Id,
+    Val,
+    LAngleBracket,
+    Text,
+}
+
+struct Lexer<'a> {
+    src: Chars<'a>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(src: &'a str) -> Self {
+        Self {
+            src: src.chars(),
+        }
+    }
+
+    fn next(&mut self) -> char {
+        self.src.next().unwrap_or('\0')
+    }
+}
+
 fn main() {
+    let input = "<h1>Hello world!</h1>";
+    let mut lexer = Lexer::new(input);
+    println!("{}", lexer.next());
 }
