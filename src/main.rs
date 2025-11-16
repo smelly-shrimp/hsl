4f347bd3a55cab2169e9e354e8a3ec48f45c66a3
Smelly Shrimp <tymek.lamza@op.pl>
2025-11-16 19:06:45 +0100

    implemented reading from file via args

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
+use std::{env, fs::{self}};
+
 use crate::lexer::Lexer;
 
 mod tok;
 mod lexer;
 
 fn main() {
-    let input = r#"<div id="this-one" class="sth">
-        <h1>Foo</h1>
-        <@desc/>
-    </div>"#;
+    let mut args = env::args();
+    args.next();
 
-    let mut lexer = Lexer::new(input);
-    lexer.lex(&input);
+    let fp = args.next().unwrap_or_else(|| {
+        panic!("no input file");
+    });
+
+    let Ok(fc) = fs::read_to_string(fp) else {
+        panic!("no such file");
+    };
+
+    // let input = r#"<div id="this-one" class="sth">
+    //     <h1>Foo</h1>
+    //     <@desc/>
+    // </div>"#;
+
+    let mut lexer = Lexer::new(&fc);
+    lexer.lex(&fc);
 }
